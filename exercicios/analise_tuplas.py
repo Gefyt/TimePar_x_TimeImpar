67dab7cef2ba10da65cfd2c259ae1c7dc88fbb9d
Jefté <Jefte238@gmail.com>
2024-03-20 12:18:20 -0300

    Atualização com listagem de preços

--- exercicios/analise_tuplas.py
+++ exercicios/analise_tuplas.py
@@ -1,24 +1,26 @@
 from random import randint
 
-cont = 0
+contPar = 0
+contImpar = 0
 valores = (randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9),
            randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9))
+
 print(f'Números sorteados: {valores}\n')
 
 print('| Time par |\n')
 for n in valores:
     if n % 2 == 0:
-        cont += n
+        contPar += n
         print(n, end='   ')
-print(f'\no time par tem = {cont} pontos')
+print(f'\no time par tem = {contPar} pontos')
 
 print('\n-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
 
 print('| Time impar |\n')
 for n in valores:
     if n % 2 != 0:
-        cont += n
+        contImpar += n
         print(n, end= '   ')
-print(f'\no time impar tem = {cont} pontos')
+print(f'\no time impar tem = {contImpar} pontos')
         
     
